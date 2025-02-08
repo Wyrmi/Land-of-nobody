118210df6332f7d1da5e9f77dabb5a291984c08a
Wyrmi <cyan.wyrmie@gmail.com>
2025-02-08 15:22:10 +0200

    Teleport player to edge

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,17 +5,38 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rb;
     public float speed;
     float dirX, dirY;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Vector3 pos;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        pos = Camera.main.WorldToViewportPoint(transform.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
         dirX = Input.GetAxis("Horizontal") * speed;
         dirY = Input.GetAxis("Vertical") * speed;
         rb.linearVelocity = new Vector2(dirX,dirY);
+    }
+    void OnBecameInvisible()
+    {
+        pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < 0.0f)
+        {
+            pos = new Vector3(1.0f, pos.y, pos.z);
+        }
+        else if (pos.x >= 1.0f)
+        {
+            pos = new Vector3(0.0f, pos.y, pos.z);
+        }
+        if (pos.y < 0.0f)
+        {
+            pos = new Vector3(pos.x, 1.0f, pos.z);
+        }
+        else if (pos.y >= 1.0f)
+        {
+            pos = new Vector3(pos.x, 0.0f, pos.z);
+        }
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
