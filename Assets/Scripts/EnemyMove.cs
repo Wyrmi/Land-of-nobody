68cff89f53acc3d155c4b4b3789c0c925b0f45f7
Wyrmi <cyan.wyrmie@gmail.com>
2025-02-09 16:36:42 +0200

    wolves teleport

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class EnemyMove : MonoBehaviour
 {
@@ -6,6 +7,7 @@ public class EnemyMove : MonoBehaviour
     public float speed;
     SpriteRenderer sr;
     Rigidbody2D rb;
+    Vector3 pos;
 
     void Start()
     {
@@ -38,5 +40,28 @@ public class EnemyMove : MonoBehaviour
     private void OnBecameVisible()
     {
         player = FindFirstObjectByType<PlayerMove>().gameObject;
+        pos = Camera.main.WorldToViewportPoint(transform.position);
+    }
+
+    void OnBecameInvisible()
+    {
+        pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < 0.0f)
+        {
+            pos = new Vector3(1.0f, pos.y, pos.z);
+        }
+        else if (pos.x >= 1.0f)
+        {
+            pos = new Vector3(0.0f, pos.y, pos.z);
+        }
+        if (pos.y < 0.0f)
+        {
+            pos = new Vector3(pos.x, 1.0f, pos.z);
+        }
+        else if (pos.y >= 1.0f)
+        {
+            pos = new Vector3(pos.x, 0.0f, pos.z);
+        }
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
